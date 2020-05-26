d11d1ab8a6c454493139d55c3e7ac750b700cfea
sendroids-young <30918357+the-youngg@users.noreply.github.com>
2020-05-26 13:57:51 +0800

    Update DateTimezoneUtil.ts
    update...

--- DateTimezoneUtil.ts
+++ DateTimezoneUtil.ts
@@ -1,37 +1,50 @@
-import {MomentInput} from "moment";
 import moment from "moment-timezone";
 
 export class DateTimezoneUtil {
 
     static DateFormat = "YYYY年MM月DD日";
     static DateTimeFormat = "YYYY年MM月DD日 HH:mm:ss";
+
     // 获取本地时区
     static LocalTimezone = moment.tz.guess();
 
-
     /**
-     * 获取带有时区的时间
+     * 格式化时间
      *
-     * @param timezone
-     * @param input
+     * @param inputTime 输入的时间   例：2020-05-26 10:38:00
+     * @param outputFormat 输出的格式    例：YYYY年MM月DD日 HH:mm:ss
+     * @return 输出时间对应的格式    例：2020年5月26日 10:38:00
      */
-    private static formatDateWithTimezone(timezone: string, input: MomentInput) {
-        return moment.tz(input, timezone);
+    public static formatDateTime(inputTime: string, outputFormat: string) {
+        return moment(inputTime).format(outputFormat);
     }
 
     /**
-     * 将其他时区的时间转换为本地时区的时间
+     * 将时间转成UTC
      *
-     * @param inputTimezone 输入的时区   例：Asia/Tokyo
-     * @param input 输入的时间   例：2020-05-20 18:00:00
-     * @return  例：Asia/Shanghai: 2020年05月20日 17:00:00
+     * @param inputTime 输入的时间   例：2020-05-26 10:38:00
+     * @param inputFormat 输入的时间的格式  例：yyyy-MM-DD HH:mm:ss
+     * @param inputTimezone 输入的时间所在的时区  例：Asia/Tokyo
+     * @return 输入的时间对应的UTC时间    例：2020-05-26T01:38:00Z
      */
-    public static formatDateToLocalTimezone(inputTimezone: string, input: MomentInput) {
-        return this.LocalTimezone +
-            ": " +
-            this.formatDateWithTimezone(inputTimezone, input)
-                .tz(this.LocalTimezone)
-                .format(this.DateTimeFormat);
+    public static formatDateToUTC(inputTime: string, inputFormat: string, inputTimezone: string) {
+        return moment.tz(inputTime, inputFormat, inputTimezone).utc().format();
     }
+
+    /**
+     * 将时间转成本地时区时间
+     *
+     * @param inputTime 输入的时间   例：2020-05-26 10:38:00
+     * @param inputFormat 输入的时间的格式  例：yyyy-MM-DD HH:mm:ss
+     * @param inputTimezone 输入的时间所在的时区  例：Asia/Tokyo
+     * @param outputFormat 输出的时间格式  例：YYYY年MM月DD日 HH:mm:ss
+     * @return 格式化后的本地时区时间  例：2020年05月26日 09:38:00
+     *
+     */
+    public static formatDateToLocal(inputTime: string, inputFormat: string, inputTimezone: string, outputFormat: string) {
+        const utcTime = this.formatDateToUTC(inputTime, inputFormat, inputTimezone);
+        return this.formatDateTime(moment(utcTime).tz(this.LocalTimezone).format(), outputFormat);
+    }
+
 
 }
